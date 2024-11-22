53ad9cd1aa3ab9337205845e24d415cb8eb07431
Pablo Gomez Barreiro <pablogomezbr@hotmail.es>
2024-11-22 23:29:05 +0000

    plot_article update

--- R/global.R
+++ R/global.R
@@ -6,4 +6,7 @@ utils::globalVariables(c(".",
                          "Received",
                          "Accepted",
                          "tat",
-                         "issue_type"))
+                         "issue_type",
+                         "year_month",
+                         "mean_tat"
+                         ))
--- R/plot_articles.R
+++ R/plot_articles.R
@@ -1,8 +1,8 @@
-#' Plots informaton obtained from article_info
+#' Plots information obtained from article_info. For analysis purposes, Editorial and Correction type articles are ignored.
 #' 
 #' @param articles_info Output dataframe from function articles_info.
 #' @param journal A string with the name of the journal for graph title purposes
-#' @param type select "summary","issues" or "type" depending on desired graph
+#' @param type select "summary","issues", "tat" or "type" depending on desired graph
 #' @import magrittr ggplot2 dplyr lubridate stringr scales
 #' @export plot_articles
 #' @return A plot (class: \code{ggplot}) depicting the desired information obtained from \code{article_info}
@@ -12,7 +12,55 @@
 
 plot_articles<-function(articles_info,journal,type){
   
+  valid_types <- c("tat", "summary", "issues", "type")
+  if (!type %in% valid_types) {
+    stop("Error: parameter type must be 'summary, 'issues', 'tat' or 'type'")
+  }
+  
   switch(type,
+         
+         "tat" = {
+           
+           tat<-articles_info%>%
+             filter(!article_type%in%c("Editorial","Correction"))%>%
+             mutate(year_month = floor_date(Accepted, "month")) %>%
+             summarize(.by=year_month, mean_tat=mean(tat))%>%
+             filter(if_all(everything(), ~ !is.na(.))) #alternative to drop_na()
+           
+           last_tat<-tat%>%
+             filter(year_month==max(year_month))%>%
+             .[[2]]
+           
+           
+           graph<- ggplot(tat,aes(x=year_month,y=mean_tat))+
+             annotate("segment", x = min(tat$year_month),xend = max(tat$year_month),y = last_tat, 
+               yend = last_tat, colour = "red", alpha = 0.7,linetype = "dotted")+
+             #geom_segment(aes(x=min(year_month),xend = max(year_month),y=last_tat,yend=last_tat),colour="red",alpha=.7,linetype = "dotted")+
+             scale_x_date(date_labels = "%Y",breaks = date_breaks("year"))+
+             geom_point(shape=21,colour="white",fill="#daeaf5",size=3)+
+             
+             scale_x_date(date_labels = "%Y",breaks = date_breaks("year"))+
+             theme_classic()+
+             geom_line(colour="white",alpha=.2,linetype="dashed")+
+             theme(text=element_text(size=20),
+                   plot.title = element_text(size=14))+
+             theme(axis.line.x = element_line(color="white", linewidth =  2),
+                   axis.line.y = element_line(color="white", linewidth =  2),
+                   axis.text.x = element_text(color="white"),
+                   axis.text.y = element_text(color="white"),
+                   legend.position = "none",
+                   text=element_text(size=16,color = "white"),
+                   panel.background = element_rect(fill = "#272822"),
+                   plot.background = element_rect(fill = "#272822"))+
+             labs(x="Accepted", y="Days", title = paste0("MDPI ",str_to_title(journal)," - Monthly Turnaround Times"),
+                  caption=paste("Last month TAT:",round(last_tat,2), "days (dotted red line)"))+
+             scale_y_continuous(limits = c(0,NA))
+           
+           
+         },
+         
+         
+         
          "summary" = {
            
            graph<- ggplot(articles_info,aes(x=Accepted,y=0))+
@@ -89,9 +137,6 @@ plot_articles<-function(articles_info,journal,type){
            
            
            
-         },
-         {
-           message("type must be 'summary, 'issues' or 'type'")
          }
   )
   
